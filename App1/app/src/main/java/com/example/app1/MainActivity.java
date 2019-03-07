cfabceb073fe1cd32f137150ebb20cc9a66461fa
Nati098 <chicken000@mail.ru>
2019-03-08 02:13:31 +0300

    add App1

---
+++ App1/app/src/main/java/com/example/app1/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.app1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button button1;
+    private EditText editText1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+
+        editText1 = (EditText) findViewById(R.id.editText1);
+        //editText1.setListener
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent1 = new Intent(this, ViewActivity.class);
+        intent1.putExtra("msg", editText1.getText().toString());
+        startActivity(intent1);
+    }
+}
---
+++ App1/app/src/main/java/com/example/app1/ViewActivity.java
@@ -0,0 +1,50 @@
+package com.example.app1;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ViewActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button button1;
+    private TextView textObject1;
+    private String msg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+
+        msg = getIntent().getStringExtra("msg");
+        textObject1 = (TextView) findViewById(R.id.textView2);
+        textObject1.setText(msg);  //getExtras().getString("msg")
+
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent1 = new Intent(Intent.ACTION_SEND);
+        if (intent1.resolveActivity(getPackageManager()) == null){
+            button1.setEnabled(false);
+            Toast.makeText(getApplicationContext(), "Can't find Email app", Toast.LENGTH_LONG).show();
+        }
+        else{
+            intent1.setData(Uri.parse("mailto:"));
+            intent1.setType("*/*");
+            intent1.putExtra(Intent.EXTRA_EMAIL, "chicken000@mail.ru");
+            intent1.putExtra(Intent.EXTRA_SUBJECT, "App1 test");
+            intent1.putExtra(Intent.EXTRA_TEXT, msg);
+
+            startActivity(intent1);
+        }
+
+
+    }
+}
