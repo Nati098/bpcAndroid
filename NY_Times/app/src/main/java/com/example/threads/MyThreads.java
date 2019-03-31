f2aca09bc1eb6e63b12b550fcf2d9fed8d881ad9
Nati098 <chicken000@mail.ru>
2019-03-31 08:55:21 +0300

    Fix some bugs (doesn't work properly yet...)

---
+++ NY_Times/app/src/main/java/com/example/threads/MyThreads.java
@@ -0,0 +1,33 @@
+package com.example.threads;
+
+public class MyThreads {
+    /*@Override
+    public void onStart() {
+        super.onStart();
+        new Thread(new LeftLeg()).start();
+        new Thread(new RightLeg()).start();
+    }
+
+    private class LeftLeg implements Runnable {
+        private boolean isRunning = true;
+        @Override
+        public void run() {
+            while (isRunning) System.out.println("Left step");
+        }
+    }
+
+    private class RightLeg implements Runnable {
+        private boolean isRunning = true;
+        @Override
+        public void run() {
+            while (isRunning) System.out.println("Right step");
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+*/
+
+}
